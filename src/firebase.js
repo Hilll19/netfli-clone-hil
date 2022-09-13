0610a5c745a21a37dccdef9d8ba1e4010860537a
Hilll19 <hilworking0110@gmail.com>
2022-09-13 20:16:14 +0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA1PZtz-DWsVyKYT5M6O8Cl5LXRABBqv7k",
+    authDomain: "netflix-clone-hil.firebaseapp.com",
+    projectId: "netflix-clone-hil",
+    storageBucket: "netflix-clone-hil.appspot.com",
+    messagingSenderId: "144077113130",
+    appId: "1:144077113130:web:d78bfd0af04228ff9d4053"
+  };
