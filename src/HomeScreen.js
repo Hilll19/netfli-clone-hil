77a5bf3a1980d3c232fe74f475d30e7781e0c7d5
Hilll19 <hilworking0110@gmail.com>
2022-09-14 12:47:37 +0700

    commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HomeScreen.css";
+
+function HomeScreen() {
+  return <div Classname="homeScreen">
+        {/* Nav */}
+        {/* Banner */}
+        {/* Row */}
+  </div>;
+  
+}
+
+export default HomeScreen
