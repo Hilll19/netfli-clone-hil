6791a30eebb0cd9cb9a1254e8f6a923865e29931
Hilll19 <hilworking0110@gmail.com>
2022-09-15 19:52:32 +0700

    commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 function Nav() {
+const [show, handleShow] = useState(false);
+
+const transitionNavBar = () ==> {
+    if (window.scrolly > 100) {
+      handleShow(true);
+    }else {
+      handleShow(false);
+    }
+}
+useEffect
   return (
-    <div className="nav">
-      <img src="https://i.pinimg.com/originals/08/68/dc/0868dcacc7c50ca2c7d35b27be81e5a9.png" alt=""/>
-      <h1>This is the Nav</h1>
+    <div className="nav nav__black">
+     <div className="nav__contents">
+       <img 
+         className="nav__logo"
+         src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
+         alt=""
+        />
+       <img 
+         className="nav__avatar"
+         src="https://pro2-bar-s3-cdn-cf1.myportfolio.com/dddb0c1b4ab622854dd81280840458d3/98032aebff601c1d993e12a0_rw_600.png?h=8030f4d5734548795c22da59ca72e3e1" 
+         alt=""
+        />
+     </div>
     </div>
   );
 }
