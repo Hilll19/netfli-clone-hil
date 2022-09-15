3bc358d2741a34e18cea088e2fb5bfe28a4bfaa0
Hilll19 <hilworking0110@gmail.com>
2022-09-15 23:33:22 +0700

    commit

--- src/Banner.js
+++ src/Banner.js
@@ -6,11 +6,21 @@ function Banner() {
     <header className="banner" 
     style={{
         backgroundSize: "cover",
-        backgroundImage: 'url("https://linitiative.ca/wp-content/uploads/2018/02/netflix-banner.jpg")',
+        backgroundImage: 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/2560px-Black_flag.svg.png")',
         backgroundPosition: "center center",
       
       }}
-    ></header>
+    >
+        <div className="banner__contents">
+            <h1 className="banner__title">Movie Name</h1>
+            <div className="bunner__buttons">
+                <button className="banner__button">Play</button>
+                <button className="banner__button">My list</button>
+            </div>
+            <h1 className="banner__description">This is a test description</h1>
+        </div>
+        <div className="banner--declaration" />
+    </header>
   );
 }
 
