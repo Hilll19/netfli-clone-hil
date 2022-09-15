f34549cd4d86730cf28b2bf009fbef8791aa49e3
Hilll19 <hilworking0110@gmail.com>
2022-09-15 19:59:07 +0700

    commit

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -6,7 +6,19 @@ function HomeScreen() {
   return (
     <div className="homeScreen">
         <Nav />
-
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
         {/* Banner */}
 
         {/* Row */}
