f6b3266f1fcf88f1d5900f259523ec6720815f55
Hilll19 <hilworking0110@gmail.com>
2022-09-15 14:34:55 +0700

    commit

--- src/Nav.js
+++ src/Nav.js
@@ -4,7 +4,7 @@ import "./Nav.css";
 function Nav() {
   return (
     <div className="nav">
-      <img src="../src/image/netflix-icon.jpg" alt="logo"/>
+      <img src="https://i.pinimg.com/originals/08/68/dc/0868dcacc7c50ca2c7d35b27be81e5a9.png" alt=""/>
       <h1>This is the Nav</h1>
     </div>
   );
